2dce3f8d4040ca0fdccd11979f03a8717ee3f84f
skalahar <skalahar@gmail.com>
2025-02-08 19:46:55 -0700

    modified new_image.sh script

--- scripts/new_image.sh
+++ scripts/new_image.sh
@@ -1,2 +1,2 @@
-python /home/skalahar/PaperPiAI/src/generate_picture.py /home/skalahar/tmp
-python /home/skalahar/PaperPiAI/src/display_picture.py /home/skalahar/tmp/output.png
+/home/skalahar/infinia-frame/venv/bin/python /home/skalahar/infinia-frame/src/generate_picture.py /home/skalahar/infinia-frame/images
+/home/skalahar/infinia-frame/venv/bin/python /home/skalahar/infinia-frame/src/display_picture.py /home/skalahar/infinia-frame/images/output.png
